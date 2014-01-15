4b86d3d3867ab40f5c3c24fe2d4332a3d146cb65
Brian Stien <brian.stien@moneydesktop.com>
2014-01-15 16:09:35 -0700

    Only patch AR base when vertica adapter loads.

--- lib/activerecord-import/active_record/adapters/vertica5_adapter.rb
+++ lib/activerecord-import/active_record/adapters/vertica5_adapter.rb
@@ -3,3 +3,13 @@ require "activerecord-import/adapters/vertica5_adapter"
 class ActiveRecord::ConnectionAdapters::VerticaAdapter
   include ActiveRecord::Import::Vertica5Adapter
 end
+
+class ActiveRecord::Base
+  def self.import(*args)
+    if args.flatten.first.is_a?(::ActiveRecord::Base)
+      bulk_insert_records(*args)
+    else
+      bulk_insert(*args)
+    end
+  end
+end
--- lib/activerecord-import/active_record/adapters/vertica6_adapter.rb
+++ lib/activerecord-import/active_record/adapters/vertica6_adapter.rb
@@ -3,3 +3,13 @@ require "activerecord-import/adapters/vertica6_adapter"
 class ActiveRecord::ConnectionAdapters::VerticaAdapter
   include ActiveRecord::Import::Vertica6Adapter
 end
+
+class ActiveRecord::Base
+  def self.import(*args)
+    if args.flatten.first.is_a?(::ActiveRecord::Base)
+      bulk_insert_records(*args)
+    else
+      bulk_insert(*args)
+    end
+  end
+end
