07befa47921c3b05e6d6870efe32d2eac71d186a
Brian Stien <brian.stien@moneydesktop.com>
2014-01-15 11:19:39 -0700

    Patch import to work with vertica
    Vertica does not support multirow insert with the SQL that activerecord
    import generates in the import method.  We implemented bulk insert in
    the vertica adapter, so all we have to do here patch the import method.

--- lib/activerecord/import/vertica.rb
+++ lib/activerecord/import/vertica.rb
@@ -8,3 +8,22 @@ module Activerecord
     end
   end
 end
+
+# AR import loads itself inside of an activesupport load hook,
+# so we have to force load the code now so that we can patch the
+# import method.
+module ActiveRecord
+  module Import
+  end
+end
+require "activerecord-import/import"
+
+class ActiveRecord::Base
+  def self.import(*args)
+    if args.flatten.first.is_a?(::ActiveRecord::Base)
+      bulk_insert_records(*args)
+    else
+      bulk_insert(*args)
+    end
+  end
+end
